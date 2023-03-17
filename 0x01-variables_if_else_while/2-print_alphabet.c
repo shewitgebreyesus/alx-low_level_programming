5499231c2fb32a3d517cbc35001f69122f732cb8
Shewitgebreyesus <shewitgebreyesus76@gmail.com>
2023-03-17 06:37:22 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(alphabet[i]);
+	putchar('\n');
+	return (0);
+}
