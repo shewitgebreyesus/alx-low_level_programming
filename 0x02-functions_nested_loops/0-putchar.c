4b38a41d1ba8a927479918ce534c810aee7d1b6f
Shewitgebreyesus <shewitgebreyesus76@gmail.com>
2023-03-21 06:12:13 -0700

    modified _putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * Description - print _putchar that is on pchar[] array  
+ * main - pchar
+ * Description: print _putchar that is on pchar[] array
+ * Return: 0  
 */
 int main(void)
 {
